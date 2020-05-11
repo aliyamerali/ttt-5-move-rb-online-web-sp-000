c91a03a7b10a6e1cc9d5c2606598ba379f5b3314
Aliya Merali <aliya.merali@flatironschool.com>
2020-05-11 02:25:31 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,8 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(position, character = "X")
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  board[position] = character
+  return board
+end
